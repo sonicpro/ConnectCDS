afe3cec438d74f210ac7501b27d60925ac86f08d
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-05-01 21:05:43 +0300

    Initial commit.

---
+++ ConnectCDS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace ConnectCDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["MyCDSServer"].ConnectionString;
+            CrmServiceClient svc = new CrmServiceClient(connectionString);
+            if (svc.IsReady)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine(svc.LastCrmError);
+            }
+        }
+    }
+}
