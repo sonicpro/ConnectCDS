a21596d8419538355eed2f4bc4cc7c52f18ac450
Volodymyr Tsurko <v-votsur@microsoft.com>
2020-05-03 14:17:35 +0300

    A couple of queries against AuroraV54367.

---
+++ ConnectCDS/OrgServiceProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace ConnectCDS
+{
+	public sealed class OrgServiceProvider : IDisposable
+	{
+		private static OrgServiceProvider instance = new OrgServiceProvider();
+		private static CrmServiceClient svc;
+
+		// Explicit static constructor to tell C# compiler
+		// not to mark type as beforefieldinit
+		static OrgServiceProvider()
+		{
+		}
+
+		private OrgServiceProvider()
+		{
+			var settings = ConfigurationManager.AppSettings;
+			var cred = new NetworkCredential(settings["UserName"], settings["Password"]);
+			svc = new CrmServiceClient(cred, AuthenticationType.AD, settings["HostName"], settings["Port"], settings["OrgName"]);
+		}
+
+		public CrmServiceClient OrgService => svc;
+
+		public static OrgServiceProvider Instance => instance;
+
+		public void Dispose()
+		{
+			svc.Dispose();
+		}
+	}
+}
--- ConnectCDS/Program.cs
+++ ConnectCDS/Program.cs
@@ -1,23 +1,50 @@
-﻿using System;
-using System.Configuration;
+﻿using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Linq;
 
 namespace ConnectCDS
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var connectionString = ConfigurationManager.ConnectionStrings["MyCDSServer"].ConnectionString;
-            CrmServiceClient svc = new CrmServiceClient(connectionString);
-            if (svc.IsReady)
-            {
-                Console.WriteLine("OK");
-            }
-            else
-            {
-                Console.WriteLine(svc.LastCrmError);
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CrmServiceClient svc = OrgServiceProvider.Instance.OrgService;
+			if (svc.IsReady)
+			{
+				var accountQuery = new QueryExpression("account");
+				accountQuery.ColumnSet = new ColumnSet(new[] { "address1_line1" });
+				var list = svc.RetrieveMultiple(accountQuery);
+				list.Entities.ToList().ForEach(a => Console.WriteLine(a.Attributes.Contains("address1_line1") ? a.Attributes["address1_line1"] :
+					"\"\""));
+
+				Guid accountId = (Guid)list.Entities.FirstOrDefault()["accountid"];
+				ContactsByAccountId(accountId, svc);
+			}
+			else
+			{
+				Console.WriteLine(svc.LastCrmError);
+			}
+		}
+
+		private static void ContactsByAccountId(Guid accountId, CrmServiceClient svc)
+		{
+			var context = new OrganizationServiceContext(svc);
+			var query = from c in context.CreateQuery("contact")
+						join a in context.CreateQuery("account") on
+							c["parentcustomerid"] equals a["accountid"]
+						where (Guid)a["accountid"] == accountId
+						select new
+						{
+							FirstName = c["firstname"] ?? "",
+							LastName = c["lastname"] ?? ""
+						};
+			foreach (var c in query)
+			{
+				Console.WriteLine($"First name: {c.FirstName}, Last name: {c.LastName}");
+			}
+		}
+	}
+
 }
